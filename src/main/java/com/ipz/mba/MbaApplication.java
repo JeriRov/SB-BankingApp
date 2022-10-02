8d7811163f59cb1811cb7312811a1424952d050e
Artem Magei <artemmagej@gmail.com>
2022-10-02 18:18:29 +0300

    initial commit

---
+++ src/main/java/com/ipz/mba/MbaApplication.java
@@ -0,0 +1,13 @@
+package com.ipz.mba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipz/mba/MbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipz.mba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
