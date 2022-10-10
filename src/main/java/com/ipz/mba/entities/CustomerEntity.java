4cf4dbb4029ad199774f16bd512f2eb5ce9645a2
Artem Magei <artemmagej@gmail.com>
2022-10-10 16:07:59 +0300

    feat: added roles for customer and create customer service

--- src/main/java/com/ipz/mba/entities/CustomerEntity.java
+++ src/main/java/com/ipz/mba/entities/CustomerEntity.java
@@ -3,6 +3,7 @@ package com.ipz.mba.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "customers")
@@ -19,16 +20,19 @@ public class CustomerEntity {
     @Column(name = "surname")
     private String lastName;
 
-    @Column(name = "premium")
-    private Boolean premium;
-
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id")
     private UserEntity userEntity;
 
-    public CustomerEntity(String firstName, String lastName, Boolean premium) {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "customer_roles",
+            joinColumns = {@JoinColumn(name = "customer_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private List<RoleEntity> roles;
+
+    public CustomerEntity(String firstName, String lastName, List<RoleEntity> roles) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.premium = premium;
+        this.roles = roles;
     }
 }
--- src/main/java/com/ipz/mba/models/ClientDataRegistration.java
+++ src/main/java/com/ipz/mba/models/ClientDataRegistration.java
@@ -1,8 +1,11 @@
 package com.ipz.mba.models;
 
 import com.ipz.mba.entities.CustomerEntity;
+import com.ipz.mba.entities.RoleEntity;
 import com.ipz.mba.entities.UserEntity;
 import lombok.*;
+
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -14,19 +17,19 @@ public class ClientDataRegistration {
     private String phoneNumber;
     private String passportNumber;
     private String password;
-    private Boolean premium;
+    private List<RoleEntity> roles;
 
 
     public static boolean hasNullFields(ClientDataRegistration clientData) {
         return clientData.getFirstName() == null || clientData.getLastName() == null ||
-                clientData.getPassword() == null || clientData.getPremium() == null;
+                clientData.getPassword() == null || clientData.roles == null;
     }
     public static UserEntity getUserEntity(ClientDataRegistration data) {
         return new UserEntity(data.getPhoneNumber(), data.getPassportNumber(), data.getPassword());
     }
 
     public static CustomerEntity getCustomerEntity(ClientDataRegistration data) {
-        return new CustomerEntity(data.getFirstName(), data.getLastName(), data.getPremium());
+        return new CustomerEntity(data.getFirstName(), data.getLastName(), data.getRoles());
     }
 
 }
