8a0fe01da91cf3c84397ddded6fdf8872b22fc2c
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2022-10-17 14:44:19 +0300

    fix(logincontroller): make it post method for login

--- src/main/java/com/ipz/mba/controllers/LoginController.java
+++ src/main/java/com/ipz/mba/controllers/LoginController.java
@@ -1,53 +1,48 @@
 package com.ipz.mba.controllers;
 
+import com.ipz.mba.models.ClientDataLogin;
 import com.ipz.mba.security.jwt.JWTUtil;
-import com.ipz.mba.services.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
-@RequestMapping("/login")
 public class LoginController {
     private final AuthenticationManager authManager;
-    private final LoginService loginService;
     private final JWTUtil jwtUtil;
 
     @Autowired
-    public LoginController(@Qualifier("authenticationManager") AuthenticationManager authManager, LoginService loginService, JWTUtil jwtUtil) {
+    public LoginController(@Qualifier("authenticationManager") AuthenticationManager authManager, JWTUtil jwtUtil) {
         this.authManager = authManager;
-        this.loginService = loginService;
         this.jwtUtil = jwtUtil;
     }
 
-    @GetMapping("/phone/{phone}/{password}")
-    public Map<String, String> loginViaPhone(@PathVariable String phone, @PathVariable String password){
-        System.out.println("LOG: Try to log in via phone " + phone);
-        var authToken = new UsernamePasswordAuthenticationToken(phone, password);
+    @PostMapping(path="/login")
+    public Map<String, String> login(@RequestBody ClientDataLogin cdl){
+        String number = "";
+        if(cdl.isPhone()){
+            log.info("LOG: Try to log in via phone {}" , cdl.getPhoneNumber());
+            number = cdl.getPhoneNumber();
+        }else{
+            log.info("LOG: Try to log in via ipn {}" , cdl.getIpn());
+            number = cdl.getIpn();
+        }
+
+        var authToken = new UsernamePasswordAuthenticationToken(number, cdl.getPassword());
         try {
             authManager.authenticate(authToken);
         } catch (BadCredentialsException ex) {
             return Map.of("error", "bad credentials");
         }
         // refreshing jwt by phoneNumber
-        String newJwtToken = jwtUtil.generateToken(phone);
+        String newJwtToken = jwtUtil.generateToken(number);
         return Map.of("jwt-token", newJwtToken);
-    }
-
-    @GetMapping("/ipn/{ipn}/{password}")
-    public ResponseEntity<String> loginViaIpn(@PathVariable String ipn, @PathVariable String password){
-        System.out.println("LOG: Try to log in via passport " + ipn);
-        if (password.equals(loginService.getPasswordByIpn(ipn)))
-            return ResponseEntity.ok("successful login");
-        return ResponseEntity.badRequest().body("not successful login");
     }
 }
