65f1202be4484e9ef82cf10d120e6f0d974f8e51
Igor <ig.zuban1ch@gmail.com>
2022-10-26 21:12:13 +0300

    feat: create TransferRequestData for making transactions

---
+++ src/main/java/com/ipz/mba/models/TransferRequestData.java
@@ -0,0 +1,23 @@
+package com.ipz.mba.models;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class TransferRequestData {
+    private String senderCardNumber;
+    private String receiverCardNumber;
+    private String receiverName;
+    private Long sum;
+    private String purpose;
+
+    public static boolean isValid(TransferRequestData data) {
+        return !(data.getSenderCardNumber() == null || data.getSenderCardNumber().isBlank() ||
+                data.getReceiverCardNumber() == null || data.getReceiverCardNumber().isBlank() ||
+                data.getReceiverName() == null || data.getReceiverName().isBlank() ||
+                data.getSum() == null || data.getSum() <= 0 || data.getPurpose() == null);
+    }
+}
