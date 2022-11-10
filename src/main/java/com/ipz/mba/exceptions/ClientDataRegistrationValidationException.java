8de4b42eb33e7e5fa3e06459de69a13eb542596a
Igor <ig.zuban1ch@gmail.com>
2022-11-10 11:21:11 +0200

    feat: add special exception for client data

---
+++ src/main/java/com/ipz/mba/exceptions/ClientDataRegistrationValidationException.java
@@ -0,0 +1,7 @@
+package com.ipz.mba.exceptions;
+
+public class ClientDataRegistrationValidationException extends Exception {
+    public ClientDataRegistrationValidationException(String message) {
+        super(message);
+    }
+}
