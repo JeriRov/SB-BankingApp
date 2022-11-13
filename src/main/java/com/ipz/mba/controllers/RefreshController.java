a68a47fbe05d3b465d285c8755458d01c0884bb6
Igor <ig.zuban1ch@gmail.com>
2022-11-13 14:47:03 +0200

    feat(RefreshController): add logout end-point

--- src/main/java/com/ipz/mba/controllers/RefreshController.java
+++ src/main/java/com/ipz/mba/controllers/RefreshController.java
@@ -1,6 +1,7 @@
 package com.ipz.mba.controllers;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.ipz.mba.entities.UserEntity;
 import com.ipz.mba.models.RefreshToken;
 import com.ipz.mba.security.jwt.JWTUtil;
 import com.ipz.mba.services.RefreshTokenService;
@@ -45,4 +46,24 @@ public class RefreshController {
                 "access_token", newAccessToken
         );
     }
+
+    @PostMapping("/logout")
+    public Map<String, String> logOut(@RequestBody RefreshToken body) {
+        String phoneNumber;
+        try {
+            phoneNumber = jwtUtil.validateRefreshToken(body.getRefreshToken());
+        } catch (JWTVerificationException ex) {
+            return Map.of("error", ex.getMessage());
+        }
+
+        UserEntity user = refreshTokenService.isUserPresentWithRefreshToken(phoneNumber, body.getRefreshToken());
+        if (user == null) {
+            return Map.of("error", "user with such refresh-token was not found");
+        }
+
+        refreshTokenService.updateRefreshToken(user, null);
+
+        return Map.of("success", "logout");
+    }
+
 }
