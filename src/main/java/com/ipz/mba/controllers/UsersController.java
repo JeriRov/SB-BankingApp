3f571b54d9f9f69351664f8dfa163ffbdb42b846
Igor <ig.zuban1ch@gmail.com>
2022-11-15 21:03:52 +0200

    feat(UsersController): create end-point for getting user info

--- src/main/java/com/ipz/mba/controllers/UsersController.java
+++ src/main/java/com/ipz/mba/controllers/UsersController.java
@@ -1,10 +1,27 @@
 package com.ipz.mba.controllers;
 
+import com.ipz.mba.entities.CustomerEntity;
+import com.ipz.mba.models.UserInfo;
+import com.ipz.mba.security.models.CustomerDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UsersController {
 
+    @GetMapping("/info")
+    public UserInfo getUserInfo() {
+        log.info("UsersController: getUserInfo()");
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomerEntity customer = ((CustomerDetails) auth.getPrincipal()).getCustomer();
+
+        return new UserInfo(customer.getFirstName(), customer.getLastName());
+    }
 }
