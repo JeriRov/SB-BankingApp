4076b8164a6f808709a3b2d121011bc34b044976
Igor <ig.zuban1ch@gmail.com>
2022-11-15 13:40:49 +0200

    feat: create model for returning all user transactions

---
+++ src/main/java/com/ipz/mba/models/RecentTransactionInfo.java
@@ -0,0 +1,54 @@
+package com.ipz.mba.models;
+
+import com.ipz.mba.entities.CardEntity;
+import com.ipz.mba.entities.TransactionEntity;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+@Slf4j
+@Getter
+public class RecentTransactionInfo {
+    private final long id;
+    private final String provider;
+    private final String currency;
+    private final ZonedDateTime time;
+    private final long sum;
+    private final boolean profit;
+
+    private RecentTransactionInfo(long id, String provider, String currency, ZonedDateTime time, long sum, boolean profit) {
+        this.id = id;
+        this.provider = provider;
+        this.currency = currency;
+        this.time = time;
+        this.sum = sum;
+        this.profit = profit;
+    }
+
+    public static RecentTransactionInfo get(CardEntity card, TransactionEntity transaction) {
+        log.info("my card: {} | receiver card: {}", card.getCardNumber(), transaction.getReceiverCardNumber());
+        return new RecentTransactionInfo(
+                transaction.getId(),
+                card.getProviderEntity().getProviderName(),
+                card.getCurrencyName(),
+                transaction.getTime(),
+                transaction.getSum(),
+                card.getCardNumber().equals(transaction.getReceiverCardNumber())
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecentTransactionInfo that = (RecentTransactionInfo) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
