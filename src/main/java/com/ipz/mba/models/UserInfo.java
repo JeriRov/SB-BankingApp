a9b1936200c98896e165bf0fa560fd9d414f511e
Igor <ig.zuban1ch@gmail.com>
2022-11-15 21:02:33 +0200

    refactor(UserInfo): remove all annotations

--- src/main/java/com/ipz/mba/models/UserInfo.java
+++ src/main/java/com/ipz/mba/models/UserInfo.java
@@ -1,8 +1,3 @@
 package com.ipz.mba.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public record UserInfo(String firstName, String lastName) { }
