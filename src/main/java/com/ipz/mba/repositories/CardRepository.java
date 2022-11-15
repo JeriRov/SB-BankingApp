e65199935b0caebf09a974d37d3d162adf07aafa
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2022-11-15 15:16:43 +0200

    feat: add all cards end point

--- src/main/java/com/ipz/mba/repositories/CardRepository.java
+++ src/main/java/com/ipz/mba/repositories/CardRepository.java
@@ -4,9 +4,11 @@ import com.ipz.mba.entities.CardEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CardRepository extends CrudRepository<CardEntity, Long> {
     Optional<CardEntity> findByCardNumber(String cardNumber);
+    List<CardEntity> findCardEntitiesByOwnerId(long ownerId);
 }
